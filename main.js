d4bff3184615389253001373a7c4d66764417c17
Costas Economou <cgeconomou@hotmail.com>
2025-06-12 08:52:20 +0300

    second commit

--- main.js
+++ main.js
@@ -1,1 +1,27 @@
 console.log('Hello, World!');
+
+const unusedVar = 42
+
+let count = 0;
+const increment = () => {
+  count++
+  console.log(`Count is now: ${count}`);
+}
+
+async function getData() {
+  try {
+    const response = await fetch('https://jsonplaceholder.typicode.com/todos/1')
+    const data = await response.json()
+    console.log("Fetched data:", data)
+  } catch(error) {
+    console.error('Fetch failed!', error)
+  }
+}
+
+increment()
+getData()
+
+if (true) {
+    const message = "This is a test"
+    console.log(message)
+}
